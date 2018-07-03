1ace222148f9ecf4174467c1b8d0ef8400cd23b8
Hendrik Iseke <hendrik.iseke@otto.de>
2018-07-03 12:09:31 +0200

    ^^

--- app/src/main/java/de/otto/ov/brain/easytouch/ScaleActivity.kt
+++ app/src/main/java/de/otto/ov/brain/easytouch/ScaleActivity.kt
@@ -23,29 +23,11 @@ class ScaleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scale)
 
-        val task = object : AsyncTask<Void, Void, String>() {
-            override fun doInBackground(vararg params: Void): String {
-                val textView = findViewById<TextView>(R.id.lightest).apply {
-                    text = getValueFromJson("http://tavira-net.servebeer.com/storage/E2C_24.json") + " kg"
-                }
-                val textView2 = findViewById<TextView>(R.id.heaviest).apply {
-                    text = getValueFromJson("http://tavira-net.servebeer.com/storage/E2C_23.json") + " kg"
-                }
-                return "test"
-            }
-
-            override fun onPostExecute(s: String) {
-                if (!isFinishing && !isCancelled) {
-                    Log.d("Request", s)
-                    Toast.makeText(this@ScaleActivity, "Request performed", Toast.LENGTH_LONG).show()
-                }
-            }
+        val textView = findViewById<TextView>(R.id.lightest).apply {
+            text = getValueFromJson("http://tavira-net.servebeer.com/storage/E2C_24.json") + " kg"
         }
-//        val textView = findViewById<TextView>(R.id.lightest).apply {
-//            text = getValueFromJson("http://tavira-net.servebeer.com/storage/E2C_24.json") + " kg"
-//        }
-//        val textView2 = findViewById<TextView>(R.id.heaviest).apply {
-//            text = getValueFromJson("http://tavira-net.servebeer.com/storage/E2C_23.json") + " kg"
-//        }
+        val textView2 = findViewById<TextView>(R.id.heaviest).apply {
+            text = getValueFromJson("http://tavira-net.servebeer.com/storage/E2C_23.json") + " kg"
+        }
     }
 }
