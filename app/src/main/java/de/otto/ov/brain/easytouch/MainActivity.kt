c100bff7144456ac109252038647dc3b535a5d45
Mathias Methner <Mathias.Methner@otto.de>
2018-07-03 12:51:10 +0200

    a button to trigger notification

--- app/src/main/java/de/otto/ov/brain/easytouch/MainActivity.kt
+++ app/src/main/java/de/otto/ov/brain/easytouch/MainActivity.kt
@@ -1,10 +1,17 @@
 package de.otto.ov.brain.easytouch
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.os.StrictMode
+import android.support.v4.app.RemoteInput
+import java.text.DateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +25,29 @@ class MainActivity : AppCompatActivity() {
 
     fun sendMessage(view: View) {
         startActivity(Intent(this, ScaleActivity::class.java))
+    }
+
+    fun createNotification(view: View) {
+
+
+        // Prepare intent which is triggered if the
+        // notification is selected
+        val intent = Intent(this, NotificationReceiverActivity::class.java)
+        val pIntent = PendingIntent.getActivity(this, System.currentTimeMillis().toInt(), intent, 0)
+
+        // Build notification
+        // Actions are just fake
+        val notification = Notification.Builder(this)
+                .setContentTitle("Awesome notification " + DateFormat.getDateInstance().format(Date()))
+                .setContentText("Awesome notification triggered by you ").setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pIntent)
+                .addAction(R.mipmap.ic_launcher, "More", pIntent)
+                .build()
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // hide the notification after its selected
+        notification.flags = notification.flags or Notification.FLAG_AUTO_CANCEL
+
+        notificationManager.notify(0, notification)
     }
 }
